3d7ba53c120d345262a6f56575e70bd6f8f15e68
Scott Moser <smoser@brickies.net>
2023-02-10 21:03:07 -0500

    Rename 'm_subsystem' to a more descriptive variable name.
    m_subsystem -> m_arm
    or
    m_subsystem -> m_claw

--- src/main/java/frc/robot/commands/ArmForward.java
+++ src/main/java/frc/robot/commands/ArmForward.java
@@ -8,32 +8,32 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
 
 public class ArmForward extends CommandBase {
-  private final Arm m_subsystem;
+  private final Arm m_arm;
 
   private boolean atFront;
 
   public ArmForward(Arm subsystem) {
-    m_subsystem = subsystem;
+    m_arm = subsystem;
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_subsystem.setMotorForward();
+    m_arm.setMotorForward();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_subsystem.checkLimits();
-    atFront = m_subsystem.queryFront();
+    m_arm.checkLimits();
+    atFront = m_arm.queryFront();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_subsystem.stopMotor();
+    m_arm.stopMotor();
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/commands/ClawSwitch.java
+++ src/main/java/frc/robot/commands/ClawSwitch.java
@@ -8,29 +8,29 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Claw;
 
 public class ClawSwitch extends CommandBase {
-  private final Claw m_subsystem;
+  private final Claw m_claw;
 
   private boolean isClosed;
   private boolean isOpen;
   private boolean startClosed;
 
   public ClawSwitch(Claw subsystem) {
-    m_subsystem = subsystem;
+    m_claw = subsystem;
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_subsystem.checkLimits();
-    isClosed = m_subsystem.queryClosed();
-    isOpen = m_subsystem.queryOpen();
+    m_claw.checkLimits();
+    isClosed = m_claw.queryClosed();
+    isOpen = m_claw.queryOpen();
 
     if (isClosed) {
-      m_subsystem.setMotorReverse();
+      m_claw.setMotorReverse();
       startClosed = true;
     } else {
-      m_subsystem.setMotorForward();
+      m_claw.setMotorForward();
       startClosed = false;
     }
   }
@@ -38,15 +38,15 @@ public class ClawSwitch extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_subsystem.checkLimits();
-    isClosed = m_subsystem.queryClosed();
-    isOpen = m_subsystem.queryOpen();
+    m_claw.checkLimits();
+    isClosed = m_claw.queryClosed();
+    isOpen = m_claw.queryOpen();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_subsystem.stopMotor();
+    m_claw.stopMotor();
   }
 
   // Returns true when the command should end.
