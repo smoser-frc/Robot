92da4efd8678a8f7b994b7fe28e5fd572390b886
Scott Moser <smoser@brickies.net>
2023-02-25 22:06:13 -0500

    Add TurnToAngle pid based turning.

--- src/main/java/frc/robot/commands/TurnToAngle.java
+++ src/main/java/frc/robot/commands/TurnToAngle.java
@@ -18,6 +18,7 @@ public class TurnToAngle extends CommandBase {
   private PIDController m_PidControl;
   private double m_angle;
   private double setPoint;
+  private boolean debug = false;
 
   /**
    * Moves the robot a specified distance forward.
@@ -29,8 +30,18 @@ public class TurnToAngle extends CommandBase {
     m_drive = drive;
     m_angle = angle;
 
-    m_PidControl =
-        new PIDController(DriveConstants.kTurnP, DriveConstants.kTurnI, DriveConstants.kTurnD);
+    double p = DriveConstants.kTurnP;
+    double i = DriveConstants.kTurnI;
+    double d = DriveConstants.kTurnD;
+    if (debug) {
+      SmartDashboard.putNumber("TurnP", p);
+      SmartDashboard.putNumber("TurnI", i);
+      SmartDashboard.putNumber("TurnD", d);
+    }
+    m_PidControl = new PIDController(p, i, d);
+
+    // Set the controller tolerance - the delta tolerance ensures the robot is stationary at the
+    // setpoint before it is considered as having reached the reference
     m_PidControl.setTolerance(
         Constants.DriveConstants.kTurnToleranceDeg,
         Constants.DriveConstants.kTurnRateToleranceDegPerS);
@@ -38,19 +49,31 @@ public class TurnToAngle extends CommandBase {
     addRequirements(drive);
   }
 
+  public TurnToAngle(double angle, DriveSubsystem drive, boolean _debug) {
+    this(angle, drive);
+    debug = _debug;
+  }
+
   @Override
   public void initialize() {
-
     double heading = m_drive.getHeading();
     double target = m_angle + heading;
-    System.out.println("At " + heading + " going to " + target);
+    if (debug) {
+      double p = SmartDashboard.getNumber("TurnP", m_PidControl.getP());
+      double i = SmartDashboard.getNumber("TurnI", m_PidControl.getI());
+      double d = SmartDashboard.getNumber("TurnD", m_PidControl.getD());
+      m_PidControl.setPID(p, i, d);
+      System.out.println(
+          "At "
+              + heading
+              + " going to "
+              + target
+              + " PID=(%0.3f, %0.3f, %0.3f)".formatted(p, i, d));
+    }
 
     setPoint = heading + m_angle;
 
     m_drive.setMaxOutput(1.0);
-
-    // Set the controller tolerance - the delta tolerance ensures the robot is stationary at the
-    // setpoint before it is considered as having reached the reference
   }
 
   @Override
@@ -58,15 +81,20 @@ public class TurnToAngle extends CommandBase {
     double heading = m_drive.getHeading();
     double output = m_PidControl.calculate(heading, setPoint);
     m_drive.arcadeDrive(0, output);
-    SmartDashboard.putNumber("Heading", heading);
-    SmartDashboard.putNumber("TurnOutput", output);
+
+    if (debug) {
+      SmartDashboard.putNumber("TurnHeading", heading);
+      SmartDashboard.putNumber("TurnOutput", output);
+      SmartDashboard.putNumber("TurnTarget", setPoint);
+    }
   }
 
   @Override
   public void end(boolean interrupted) {
     m_drive.setMaxOutput(1);
-    System.out.println(
-        "At " + m_drive.getHeading() + " target " + setPoint + " Done!");
+    if (debug) {
+      System.out.println("At " + m_drive.getHeading() + " target " + setPoint + " Done!");
+    }
   }
 
   @Override
