3ad710be8f99b494829320bdf45d716b1539ab3e
Scott Moser <smoser@brickies.net>
2023-02-25 22:07:26 -0500

    Change arcade drive command to be normal arcade drive.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -36,12 +36,10 @@ public class RobotContainer {
     // Configure default commands
     // Set the default drive command to split-stick arcade drive
     m_robotDrive.setDefaultCommand(
-        // A split-stick arcade command, with forward/backward controlled by the left
-        // hand, and turning controlled by the right.
         new RunCommand(
             () ->
                 m_robotDrive.arcadeDrive(
-                    -m_driverController.getLeftY(), -m_driverController.getRightX()),
+                    -m_driverController.getLeftY(), -m_driverController.getLeftX()),
             m_robotDrive));
   }
 
