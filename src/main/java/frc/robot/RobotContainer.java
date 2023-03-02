9d3d29a7774a2db1400ce70f7f214c5bad311947
robo7660 <ssmoser+bytingirish@gmail.com>
2023-03-02 18:39:44 -0500

    TestDriveDistance in Auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -106,6 +106,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return new ExampleCommand(m_exampleSubsystem);
+    return new DriveDistance(5.486, m_drive);
   }
 }
