bbd4e458f43fb98fee5c4b2194e70060286282ab
Scott Moser <smoser@brickies.net>
2023-03-17 08:53:41 -0400

    Add the other 2 motors and connect motors via MotorControllerGroup.
    This renames the existing motors from 'm_leftMotor' to 'm_leftMotor1'
    (and m_rightMotor to m_rightMotor1) and then adds two new motors
    m_leftMotor2 and m_rightMotor2.
    
    Then, we add those two motors to a MotorControllerGroup.
    MotorControllerGroup is just what it sounds like.  Its a group of
    Motors that will be controlled as if they are one motor.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,15 +10,20 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
  * arcade steering.
  */
 public class Robot extends TimedRobot {
-  private final CANSparkMax m_leftMotor = new CANSparkMax(1, MotorType.kBrushed);
-  private final CANSparkMax m_rightMotor = new CANSparkMax(3, MotorType.kBrushed);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+  private final CANSparkMax m_leftMotor1 = new CANSparkMax(1, MotorType.kBrushed);
+  private final CANSparkMax m_leftMotor2 = new CANSparkMax(2, MotorType.kBrushed);
+  private final CANSparkMax m_rightMotor1 = new CANSparkMax(3, MotorType.kBrushed);
+  private final CANSparkMax m_rightMotor2 = new CANSparkMax(4, MotorType.kBrushed);
+  private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
+  private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
   private final Joystick m_stick = new Joystick(0);
 
   @Override
@@ -26,7 +31,7 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    m_rightMotors.setInverted(true);
   }
 
   @Override
