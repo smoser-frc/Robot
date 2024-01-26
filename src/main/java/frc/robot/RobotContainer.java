cc823a91bd56ef02c6a98567214319652e516f4a
robo7660 <98061415+robo7660@users.noreply.github.com>
2024-01-25 22:29:28 -0500

    added intake subsystem and command (#98)
    * add Intake subsystem
    * added ToggleIntake Command
    * configure x to run toggleIntake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,9 +11,12 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.AbsoluteDrive;
+import frc.robot.commands.ToggleIntake;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.SwerveSubsystem;
 import java.io.File;
 
@@ -27,6 +30,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final SwerveSubsystem m_swerve =
       new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  private final Intake m_intake = new Intake();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandJoystick joystick1 = new CommandJoystick(1);
@@ -61,7 +65,10 @@ public class RobotContainer {
    * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
-  private void configureBindings() {}
+  private void configureBindings() {
+    JoystickButton x3 = new JoystickButton(driverXbox, XboxController.Button.kX.value);
+    x3.onTrue(new ToggleIntake(m_intake));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new Intake. */
+  private CANSparkMax motorLeft = new CANSparkMax(30, CANSparkLowLevel.MotorType.kBrushless);
+
+  private CANSparkMax motorCenter = new CANSparkMax(31, CANSparkLowLevel.MotorType.kBrushless);
+  private CANSparkMax motorRight = new CANSparkMax(32, CANSparkLowLevel.MotorType.kBrushless);
+
+  double speed = 0.35;
+  double currentSpeed = 0.0;
+
+  public Intake() {
+    motorLeft.setInverted(false);
+    motorCenter.setInverted(false);
+    motorRight.setInverted(true);
+    stop();
+  }
+
+  private void set(double power) {
+    motorLeft.set(power);
+    motorCenter.set(power);
+    motorRight.set(power);
+    currentSpeed = power;
+  }
+
+  public void toggle() {
+    if (currentSpeed == 0.0) {
+      start();
+    } else {
+      stop();
+    }
+  }
+
+  public void start() {
+    set(speed);
+  }
+
+  public void reverse() {
+    set(-speed);
+  }
+
+  public void stop() {
+    set(0.0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
