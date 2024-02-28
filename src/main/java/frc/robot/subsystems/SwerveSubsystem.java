bec6ba4f61254dc4b33f68fd5cd9d35ecff5c700
Scott Moser <smoser@brickies.net>
2024-02-27 23:22:11 -0500

    spotless

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -14,7 +14,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
@@ -360,7 +359,8 @@ public class SwerveSubsystem extends SubsystemBase {
         yInput,
         headingX,
         headingY,
-        getHeading().getRadians() * (RobotBase.isSimulation() || DriverStation.isAutonomous() ? 1 : -1),
+        getHeading().getRadians()
+            * (RobotBase.isSimulation() || DriverStation.isAutonomous() ? 1 : -1),
         maximumSpeed);
   }
 
